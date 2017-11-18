add6be6ad264bcbf1892d4538878975f5b67fdc7
Niall Hunt <NiallEHunt@gmail.com>
2017-11-18 15:03:36 +0000

    Readme

--- HuntingForBikeNumbers.py
+++ HuntingForBikeNumbers.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import train_test_split
 
 data = pd.read_csv("data", index_col=0)
 
@@ -7,4 +9,23 @@ print(data.head())
 print(data.tail())
 print(data.shape)
 
-sns.pairplot(data, x_vars=['Weekday','Month','AWND','PRCP','SNOW','SNWD','TAVG','TMAX','TMIN','Holiday'], y_vars='Bikes', size=7, aspect=0.7, kind='reg')
+#,ID,Weekday,Month,AWND,PRCP,SNOW,SNWD,TAVG,TMAX,TMIN,Holiday,Bikes
+
+#sns.pairplot(data, x_vars=['TMAX', 'SNOW', 'TAVG'], y_vars='Bikes', size=7, aspect=0.7, kind='reg')
+#plt.show()
+
+X = data[['Weekday', 'Month', 'AWND', 'PRCP', 'SNOW', 'TAVG', 'TMAX', 'TMIN']]
+
+print(X.head())
+
+Y = data[['Bikes']]
+
+print(type(X))
+print(X.shape)
+
+print(Y.head())
+
+print(type(Y))
+print(Y.shape)
+
+
